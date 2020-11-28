ae11bc4f7f2700f9ac72eb202cccee6ddce97f15
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2020-11-28 17:15:58 +0100

    Update GitHub pages

---
+++ precache-manifest.5f04be9b4c12e0a481c696c873ad5055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52478b6cf1bbae3b8baed7e6522a09d0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4cc43e47dc9bedc18a0b.bundle.js"
+  },
+  {
+    "url": "runtime~main.4cc43e47dc9bedc18a0b.bundle.js"
+  },
+  {
+    "revision": "21d8cbe98e412a47cd34",
+    "url": "static/css/main.a70479fd.chunk.css"
+  },
+  {
+    "revision": "245d5b693caa3b211a5b",
+    "url": "static/css/vendors~main.3d71c16b.chunk.css"
+  },
+  {
+    "url": "vendors~main.4cc43e47dc9bedc18a0b.bundle.js"
+  },
+  {
+    "url": "vendors~main.4cc43e47dc9bedc18a0b.bundle.js.LICENSE.txt"
+  }
+]);
